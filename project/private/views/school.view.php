3db5e4367ae496d66eb021ceaceb8e585d2b0916
Shibli Raihan <shibli.raihan.bangli@gmail.com>
2023-10-25 23:49:53 +0600

    school view error done

--- project/private/views/school.view.php
+++ project/private/views/school.view.php
@@ -16,11 +16,14 @@
                             <div class="d-flex flex-row card-body profile-card pt-4 align-items-center justify-content-around">
 
                                 <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-
-                                <h2 class=""></h2><?php echo $row->school ?> </h2>
+                                <div class="d-flex flex-column">
+                                    <h5 class=""><?php echo $row->school ?> </h5>
+                                    <h6><?php echo $row->user_id  ?></h6>
+                                    <span><?php echo $row->date ?></span>
+                                </div>
                                 <h6 class=" text-end fw-bold"></h6>
                                 <button class="btn btn-light rounded-1 text-decoration-underline profile">more..</button>
-                                
+
                             </div>
                         </div>
                     </div>
